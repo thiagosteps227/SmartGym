8679ee88aed953845517dc354593765f9c3adb44
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-01-21 14:24:17 +0000

    Create Customer.java

---
+++ src/Customer.java
@@ -0,0 +1,127 @@
+package com.gym.model;
+
+import java.util.Date;
+
+public class Customer {
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String password;
+	private long phoneNumber;
+	private char gender;
+	private Date dateOfBirth;
+	
+	public Customer(String firstName,String lastName,String email, String password,long phoneNumber,
+		 char gender, Date dateOfBirth		) {
+		this.firstName=firstName;
+		this.lastName=lastName;
+		this.email=email;
+		this.password=password;
+		this.phoneNumber=phoneNumber;
+		this.gender=gender;
+		this. dateOfBirth= dateOfBirth;
+		
+	}
+
+	/**
+	 * @return the firstName
+	 */
+	public String getFirstName() {
+		return firstName;
+	}
+
+	/**
+	 * @param firstName the firstName to set
+	 */
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	/**
+	 * @return the lastName
+	 */
+	public String getLastName() {
+		return lastName;
+	}
+
+	/**
+	 * @param lastName the lastName to set
+	 */
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	/**
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * @param email the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the phoneNumber
+	 */
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	/**
+	 * @param phoneNumber the phoneNumber to set
+	 */
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	/**
+	 * @return the gender
+	 */
+	public char getGender() {
+		return gender;
+	}
+
+	/**
+	 * @param gender the gender to set
+	 */
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+
+	/**
+	 * @return the dateOfBirth
+	 */
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	/**
+	 * @param dateOfBirth the dateOfBirth to set
+	 */
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+	
+	
+	
+
+}
