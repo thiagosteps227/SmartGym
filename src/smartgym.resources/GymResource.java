6bb651337dd63d425150feb4096b701fec995b76
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-01 10:57:19 +0000

    Update GymResource.java

--- src/smartgym.resources/GymResource.java
+++ src/smartgym.resources/GymResource.java
@@ -1,16 +1,28 @@
 package smartgym.resources;
 
+import javax.websocket.server.PathParam;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/customer")
+import com.sun.jersey.api.view.Viewable;
+
+@Path("/register")
 public class GymResource {
 		
 	@GET
-	@Produces("text/plain")
-	public String getIndex() {
-		return "index.html";
+	@Path(value = "register.html")
+	public Viewable get(@PathParam("register.html") String template) {
+		return new Viewable(template, this);
 	}
+	
+	@POST
+    @Consumes({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    public Customer create(Customer customer) {
+        return dao.create(customer);
+    }
 }
