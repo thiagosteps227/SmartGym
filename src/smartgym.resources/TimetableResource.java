055ea8d9e8c1a08f064368ed40488444b2fbbef7
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-10 16:05:55 +0000

    update to find by name method

--- src/smartgym.resources/TimetableResource.java
+++ src/smartgym.resources/TimetableResource.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/timetable")
 public class TimetableResource {
@@ -18,5 +20,20 @@ public class TimetableResource {
 		return timetableDAO.findAll();
 	}
 	
+	@GET @Path("{classCode}")
+	@Produces({ MediaType.APPLICATION_JSON})
+	//http://localhost:8080/SmartGym/rest/timetable/{classCode}
+	public Response findById(@PathParam("classCode") int classCode) {
+		Timetable timetable = timetableDAO.findById(classCode);
+		return Response.status(200).entity(timetable).build();
+	}
+	
+	@GET @Path("search/{query}")
+	@Produces({MediaType.APPLICATION_JSON})
+	//http://localhost:8080/SmartGym/rest/timetable/search/{query}
+	public Response findByName(@PathParam("query") String className) {
+		List<Timetable> timetable = timetableDAO.findByName(className);
+		return Response.status(200).entity(timetable).build();
+	}
 }
 	
