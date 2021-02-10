7f4bc4993296638e642068ffd10f490fd6baeb7b
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-10 16:04:48 +0000

    update until findByName method

--- src/smartgym.resources/TimetableDAO.java
+++ src/smartgym.resources/TimetableDAO.java
@@ -7,6 +7,8 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.mysql.jdbc.PreparedStatement;
+
 public class TimetableDAO {
 
 	public List<Timetable> findAll() {
@@ -27,6 +29,48 @@ public class TimetableDAO {
 			ConnectionHelper.close(c);
 		}
         return list;
+    }
+	
+	public Timetable findById(int classCode) {
+    	String sql = "SELECT * FROM timetable WHERE classCode=?";
+    	Timetable timetable = null;
+    	Connection c = null;
+    	try {
+    		c = ConnectionHelper.getConnection();
+    		PreparedStatement ps = (PreparedStatement) c.prepareStatement(sql);
+    		ps.setInt(1, classCode);
+    		ResultSet rs = ps.executeQuery();
+    		if(rs.next()) {
+    			timetable = processRow(rs);
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		throw new RuntimeException(e);
+    	} finally {
+    		ConnectionHelper.close(c);
+    	}
+    	return timetable;
+    }
+	
+	public List<Timetable> findByName(String className) {
+    	List<Timetable> list = new ArrayList<Timetable>();
+    	Connection c= null;
+    	String sql = "SELECT * FROM timetable AS e WHERE UPPER(className) LIKE ? ORDER BY className";
+    	try {
+    		c = ConnectionHelper.getConnection();
+    		java.sql.PreparedStatement ps = c.prepareStatement(sql);
+    		ps.setString(1, "%"+className.toUpperCase()+"%");
+    		ResultSet rs = ps.executeQuery();
+    		while(rs.next()) {
+    			list.add(processRow(rs));
+    		}
+    	} catch (SQLException e){
+    		e.printStackTrace();
+    		throw new RuntimeException(e);
+    	} finally {
+    		ConnectionHelper.close(c);
+    	}
+    	return list;
     }
 
    protected Timetable processRow(ResultSet rs) throws SQLException {
