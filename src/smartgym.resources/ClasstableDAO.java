5e3590cbbfe9fea55f47b3defe6b125ec801d4a4
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-10 15:47:04 +0000

    Update with CRUD methods

--- src/smartgym.resources/ClasstableDAO.java
+++ src/smartgym.resources/ClasstableDAO.java
@@ -6,9 +6,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
-
-
-
+import com.mysql.jdbc.PreparedStatement;
 
 public class ClasstableDAO {
 	
@@ -31,6 +29,116 @@ public class ClasstableDAO {
 			}
 	        return list;
 	    }
+	   
+	   public Classtable findById(int id) {
+	    	String sql = "SELECT * FROM classtable WHERE classID=?";
+	    	Classtable classtable = null;
+	    	Connection c = null;
+	    	try {
+	    		c = ConnectionHelper.getConnection();
+	    		PreparedStatement ps = (PreparedStatement) c.prepareStatement(sql);
+	    		ps.setInt(1, id);
+	    		ResultSet rs = ps.executeQuery();
+	    		if(rs.next()) {
+	    			classtable = processRow(rs);
+	    		}
+	    	} catch (Exception e) {
+	    		e.printStackTrace();
+	    		throw new RuntimeException(e);
+	    	} finally {
+	    		ConnectionHelper.close(c);
+	    	}
+	    	return classtable;
+	    }
+	   
+	   public List<Classtable> findByName(String className) {
+	    	List<Classtable> list = new ArrayList<Classtable>();
+	    	Connection c= null;
+	    	String sql = "SELECT * FROM classtable AS e WHERE UPPER(className) LIKE ? ORDER BY className";
+	    	try {
+	    		c = ConnectionHelper.getConnection();
+	    		java.sql.PreparedStatement ps = c.prepareStatement(sql);
+	    		ps.setString(1, "%"+className.toUpperCase()+"%");
+	    		ResultSet rs = ps.executeQuery();
+	    		while(rs.next()) {
+	    			list.add(processRow(rs));
+	    		}
+	    	} catch (SQLException e){
+	    		e.printStackTrace();
+	    		throw new RuntimeException(e);
+	    	} finally {
+	    		ConnectionHelper.close(c);
+	    	}
+	    	return list;
+	    }
+	   
+	   public Classtable create(Classtable classtable) {
+	    	Connection c= null;
+	    	java.sql.PreparedStatement ps = null;
+	    	try {
+	    		c = ConnectionHelper.getConnection();
+	    		ps = c.prepareStatement("INSERT INTO classtable "
+	    				+ "(className, personLimit, pricePerClass, priceTwelveWeeks)"
+	    				+ "VALUES(?,?,?,?)", 
+	    			new String[] { "classID"});
+	    		ps.setString(1, classtable.getClassName());
+	    		ps.setInt(2, classtable.getPersonLimit());
+	    		ps.setInt(3, classtable.getPricePerClass());
+	    		ps.setInt(4, classtable.getPriceTwelveWeeks());
+	    		ps.executeUpdate();
+	    		ResultSet rs = ps.getGeneratedKeys();
+	    		rs.next();
+	    		int classID = rs.getInt(1);
+	    		classtable.setClassID(classID);
+	    	} catch (Exception e){
+	    		e.printStackTrace();
+	    		throw new RuntimeException(e);
+	    	} finally {
+	    		ConnectionHelper.close(c);
+	    	}
+	    	
+	    	return classtable;
+	    }
+	   
+	   public Classtable update(Classtable classtable) {
+	    	Connection c = null;
+	    	try {
+	    		c = ConnectionHelper.getConnection();
+	    		PreparedStatement ps = (PreparedStatement) c.prepareStatement
+	    				("UPDATE classtable SET className=?, personLimit=?, pricePerClass=?,"
+	    				+ "priceTwelveWeeks=? WHERE classID=?");
+	    		ps.setString(1, classtable.getClassName());
+	    		ps.setInt(2, classtable.getPersonLimit());
+	    		ps.setInt(3, classtable.getPricePerClass());
+	    		ps.setInt(4, classtable.getPriceTwelveWeeks());
+	    		ps.setInt(5, classtable.getClassID());
+	    		ps.executeUpdate();
+	    	} catch (SQLException e) {
+	    		e.printStackTrace();
+	    		throw new RuntimeException (e);
+	    	} finally {
+	    		ConnectionHelper.close(c);
+	    	}
+	    	return classtable;
+	    }
+	   
+	   public boolean remove(int classID) {
+	    	Connection c= null;
+	    	try {
+	    		c = ConnectionHelper.getConnection();
+	    		PreparedStatement ps = (PreparedStatement) c.prepareStatement("DELETE FROM classtable WHERE classID =?");
+	    		ps.setInt(1, classID);
+	    		int count = ps.executeUpdate();
+				return count == 1;
+	    	} catch (Exception e) {
+	    		e.printStackTrace();
+	    		throw new RuntimeException (e);
+	    	} finally {
+	    		ConnectionHelper.close(c);
+	    	}
+	    }
+
+
 
 	   protected Classtable processRow(ResultSet rs) throws SQLException {
 	        Classtable classtable = new Classtable();
