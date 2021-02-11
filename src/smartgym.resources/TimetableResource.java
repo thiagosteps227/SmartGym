3b5035bec2b6b38b083536dcf996d5c3fc7d1fa2
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-11 11:52:27 +0000

    updated with update and delete methods

--- src/smartgym.resources/TimetableResource.java
+++ src/smartgym.resources/TimetableResource.java
@@ -2,7 +2,11 @@ package com.ait.smartgym;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -34,6 +38,32 @@ public class TimetableResource {
 	public Response findByName(@PathParam("query") String className) {
 		List<Timetable> timetable = timetableDAO.findByName(className);
 		return Response.status(200).entity(timetable).build();
+	}
+	
+	@POST
+	@Consumes({ MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
+	//http://localhost:8080/SmartGym/rest/timetable
+	public Response create(Timetable timetable) {
+		Timetable timetableObj = timetableDAO.create(timetable);
+		return Response.status(201).entity(timetableObj).build();
+	}
+	
+	@PUT @Path("{classCode}")
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
+	//http://localhost:8080/SmartGym/rest/timetable/{classCode}
+	public Response update(Timetable timetable) {
+		timetableDAO.update(timetable);
+		return Response.status(201).entity(timetable).build();
+	}
+	
+	@DELETE @Path("{classCode}")
+	@Produces({MediaType.APPLICATION_JSON})
+	//http://localhost:8080/SmartGym/rest/timetable/{classCode}
+	public Response remove(@PathParam("classCode") int classCode) {
+		timetableDAO.remove(classCode);
+		return Response.status(204).build();
 	}
 }
 	
