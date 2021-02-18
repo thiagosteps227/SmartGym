f61cdfcadc39d0d9837e884910c2fbccb8cec96e
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-18 09:20:20 +0000

    Add files via upload

---
+++ src/smartgym.resources/com.ait.validation/TimetableValidationException.java
@@ -0,0 +1,12 @@
+package com.ait.validation;
+
+
+public class TimetableValidationException extends TimetableException {
+
+	private static final long serialVersionUID = 334051992916748022L;
+
+	public TimetableValidationException(final String errorMessage) {
+		super(errorMessage);
+	}
+
+}
---
+++ src/smartgym.resources/com.ait.validation/TimetableValidator.java
@@ -0,0 +1,40 @@
+package com.ait.validation;
+
+import java.util.Arrays;
+import java.util.List;
+import com.ait.smartgym.Timetable;
+
+
+public class TimetableValidator {
+	
+	Timetable timetable = new Timetable();
+	List<String> instructors = Arrays.asList("DAVID BLACK", "AUDREY CLARK","JOHN OHANA", "FRANCINE KURTZ","PAUL BYRNE");
+	
+	public void validateTimetable(Timetable timetable) throws TimetableValidationException {
+		this.timetable = timetable;
+		checkEmptyFields(timetable);
+		checkValidInstructor(timetable, instructors);
+	
+	}
+	
+	private void checkEmptyFields(Timetable timetable) throws TimetableValidationException {
+		if ((timetable.getClassName().length() == 0) || (timetable.getInstructor().length() == 0) || (timetable.getWeekDay().length() == 0)
+				|| (timetable.getClassTime().length() == 0)) {
+			throw new TimetableValidationException(ErrorMessages.EMPTY_FIELDS.getMsg());
+		}
+	}
+
+	private void checkValidInstructor(Timetable timetable, List<String> instructors) throws TimetableValidationException {
+		boolean instructorOK = false;
+		String instructorEntered = timetable.getInstructor().toUpperCase();
+		for (String instructor : instructors) {
+			if (instructor.equals(instructorEntered)) {
+				instructorOK = true;
+				break;
+			}
+		}
+		if (instructorOK == false) {
+			throw new TimetableValidationException(ErrorMessages.INVALID_INSTRUCTOR.getMsg());
+		}
+	}
+}
