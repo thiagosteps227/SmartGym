833f8034ae79a3e454591a520adc4934450950d1
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-02-25 11:04:07 +0000

    Add files via upload

---
+++ src/smartgym.dao/DateTimeAdapter.java
@@ -0,0 +1,24 @@
+package smartgym.dao;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+
+public class DateTimeAdapter extends XmlAdapter<String, Date>{
+    private final DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+
+    @Override
+    public Date unmarshal(String xml) throws Exception {
+        return dateFormat.parse(xml);
+    }
+
+    @Override
+    public String marshal(Date object) throws Exception {
+        return dateFormat.format(object);
+    }
+
+}
+
+
