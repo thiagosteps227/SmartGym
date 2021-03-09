9c4a900527942adce4d075dc84d595c0f07b502f
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-03-09 14:35:31 +0000

    with update method done

--- src/smartgym.dao/CustomerDao.java
+++ src/smartgym.dao/CustomerDao.java
@@ -9,6 +9,8 @@ import java.sql.Statement;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
+
+import smartgym.model.Classtable;
 import smartgym.model.Customer;
 
 
@@ -53,10 +55,10 @@ public class CustomerDao {
 			ResultSet rs = s.executeQuery(sql);
 			while (rs.next()) {
 				Customer customer = new Customer();
+				customer.setCustomerID(rs.getInt("CustomerID"));
 				customer.setUserName(rs.getString("userName"));
 				customer.setFirstName(rs.getString("firstName"));
 				customer.setLastName(rs.getString("lastName"));
-				
 				customer.setPassword(rs.getString("password"));
 				customer.setPhoneNumber(rs.getString("phoneNumber"));
 				customer.setEmail(rs.getString("email"));
@@ -76,6 +78,27 @@ public class CustomerDao {
 
 		return list;
 	}
+	
+	 public Customer findById(int customerID) {
+	    	String sql = "SELECT * FROM customer WHERE customerID=?";
+	    	Customer customer = null;
+	    	Connection c = null;
+	    	try {
+	    		c = ConnectionHelper.getConnection();
+	    		PreparedStatement ps = (PreparedStatement) c.prepareStatement(sql);
+	    		ps.setInt(1, customerID);
+	    		ResultSet rs = ps.executeQuery();
+	    		if(rs.next()) {
+	    			customer = processRow(rs);
+	    		}
+	    	} catch (Exception e) {
+	    		e.printStackTrace();
+	    		throw new RuntimeException(e);
+	    	} finally {
+	    		ConnectionHelper.close(c);
+	    	}
+	    	return customer;
+	    }
 	public String retrievePassword(String userName) {
 		String pwd = null;
 		Connection c = null;
@@ -97,5 +120,63 @@ public class CustomerDao {
 		}
 		return pwd;
 	}
+	
+	 public Customer update(Customer customer) {
+	    	Connection c = null;
+	    	try {
+	    		c = ConnectionHelper.getConnection();
+	    		PreparedStatement ps = (PreparedStatement) c.prepareStatement
+	    				("UPDATE customer SET userName=?, firstName=?, lastName=?,password=?, phonenumber=?,"
+	    				+ "gender=?, dateOfBirth=?, email=?, activity=? WHERE customerID=?");
+	    		ps.setString(1, customer.getUserName());
+	    		ps.setString(2, customer.getFirstName());
+	    		ps.setString(3, customer.getLastName());
+	    		ps.setString(4, customer.getPassword());
+	    		ps.setString(5, customer.getPhoneNumber());
+	    		ps.setString(6, customer.getGender());
+	    		ps.setString(7, customer.getDateOfBirth());
+	    		ps.setString(8, customer.getEmail());
+	    		ps.setString(9, customer.getActivity());
+	    		ps.setInt(10, customer.getCustomerID());
+	    		ps.executeUpdate();
+	    	} catch (SQLException e) {
+	    		e.printStackTrace();
+	    		throw new RuntimeException (e);
+	    	} finally {
+	    		ConnectionHelper.close(c);
+	    	}
+	    	return customer;
+	    }
+	
+	public boolean remove(int customerID) {
+    	Connection c= null;
+    	try {
+    		c = ConnectionHelper.getConnection();
+    		PreparedStatement ps = (PreparedStatement) c.prepareStatement("DELETE FROM customer WHERE customerID =?");
+    		ps.setInt(1, customerID);
+    		int count = ps.executeUpdate();
+			return count == 1;
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		throw new RuntimeException (e);
+    	} finally {
+    		ConnectionHelper.close(c);
+    	}
+    }
 
+	protected Customer processRow(ResultSet rs) throws SQLException {
+        Customer customer = new Customer();
+        customer.setCustomerID(rs.getInt("customerID"));
+        customer.setUserName(rs.getString("userName"));
+        customer.setFirstName(rs.getString("firstName"));
+        customer.setLastName(rs.getString("lastName"));
+        customer.setEmail(rs.getString("email"));
+        customer.setPassword(rs.getString("password"));
+        customer.setPhoneNumber(rs.getString("phoneNumber"));
+        customer.setGender(rs.getString("gender"));
+        customer.setDateOfBirth(rs.getString("dateOfBirth"));
+        customer.setActivity(rs.getString("activity"));
+       
+        return customer;
+    }
 }
