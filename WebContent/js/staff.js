4e3354527c455d08f06aeacde3d070eeb1694d6b
Thiago dos Passos <64388418+thiagosteps227@users.noreply.github.com>
2021-03-09 12:37:25 +0000

    with update method working

--- WebContent/js/staff.js
+++ WebContent/js/staff.js
@@ -1,5 +1,7 @@
 var allCustomersURL = "http://localhost:8080/SmartGym/rest/customer";
 
+var currentCustomer;
+
 var findAllCustomers = function(){
 	$.ajax({
 		type: 'GET',
@@ -9,58 +11,126 @@ var findAllCustomers = function(){
 	});
 };
 
-var findById= function(id) {
-	console.log('findById: ' + id);
+var findById= function() {
+	console.log('findById: ' +customerID);
 	$.ajax({
 		type: 'GET',
-		url: allCustomersURL + '/' + id,
+		url: allCustomersURL + '/' + $('#customerID').val(),
 		dataType: "json",
 		success: function(data){
-			$('#deleteCustomer').show();
-			console.log('findById success: ' + data.name);
+			$('#updateCustomer').show();
+			console.log('findById success: ' + data.userName);
 			currentCustomer = data;
 			renderDetails(currentCustomer);
 		}
 	});
 };
 
-var renderDetails=function(customer) {
-	$('#customerID').val(customer.customerID);
-
+var updateCustomer= function () {
+	$.ajax({
+		type: 'PUT',
+		contentType: 'application/json',
+		url: allCustomersURL  + '/' + $('#customerID').val(),
+		dataType: "json",
+		data: formToJSON(),
+		success: function(data, textStatus, jqXHR){
+			findAllCustomers();
+			alert('Customer updated successfully');
+            currentCustomer = data;
+			renderDetails(currentCustomer);
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+			alert('updateCustomer error: ' + textStatus);
+		}
+	});
 };
+
+var deleteCustomer=function() {
+	console.log('deleteCustomer');
+	$.ajax({
+		type: 'DELETE',
+		url: allCustomersURL + '/' + $('#customerID').val(),
+		success: function(data, textStatus, jqXHR){
+			alert('customer deleted successfully');
+            findAllCustomers();
+            $('#customerID').val("");
+		},
+		error: function(jqXHR, textStatus, errorThrown){
+			alert('deleteWine error');
+		}
+	});
+};
+
+var renderDetails=function(customer) {
+	$('#userName').val(customer.userName);
+	$('#firstName').val(customer.firstName);
+	$('#lastName').val(customer.lastName);
+	$('#email').val(customer.email);
+	$('#password').val(customer.password);
+	$('#phoneNumber').val(customer.phoneNumber);
+	$('#gender').val(customer.gender);
+	$('#dateOfBirth').val(customer.dateOfBirth);
+};
+
+var formToJSON = function (){
+	var customerID = $('#customerID').val();
+	return JSON.stringify({
+		"customerID": customerID == "" ? null : customerID, 
+			"userName" : $('#userName').val(),
+			"firstName" : $('#firstName').val(),
+			"lastName" : $('#lastName').val(),
+			"password" : $('#password').val(),
+			"phoneNumber" : $('#phoneNumber').val(),
+			"gender" : $('#gender').val(),
+			"dateOfBirth" : $('#dateOfBirth').val(),
+			"email" : $('#email').val(),
+			"activity" : $("input[type='checkbox']:checked").val()
+	});
+}
+
 //shows a list of all clients registered in the database
-function showRegisteredCustomers(customer) {
-	$.each(customer, function(index, customer){
-		//$('#customerTable').append('<li><a href="#" id="' + customer.id + '">'+customer.firstName+'</a></li>')
-		$('#customerTable').append('<tr><td>'+customer.userName+'</td><td>'+customer.firstName+'</td><td>'+customer.email+'</td></tr>' );
+var showRegisteredCustomers = function(list) {
+	$('#customerTable tr').remove();
+	$.each(list, function(index, customer){
+		$('#customerTable').append('<tr><td>'+customer.customerID+'</td><td>'+customer.userName+'</td><td>'+customer.email+'</td></tr>' );
 	});	
 }
 
-$(document).on("click", '#customerTable a', function(){
-	findById(this.id);
-	
-	});
 
 var showAllCustomers = function(){
 	findAllCustomers();
 	$('div[id="customerDiv"]').show();
 	$(this).hide();
 	$('#deleteCustomer').show();
+	$('div[id="deleteFormDiv"]').show();
+	$('#showCustomerDetailsButton').show();
+	$('#updateForm').show();
 }
 
-var deleteCustomers = function(){
-	$('div[id="deleteDiv"]').show();
-	
+
+var showCustomerDetails = function(){
+	findById();
+	$('div[id="customerDiv"]').hide();
+	$('div[id="updateForm"]').show();
+	$('#showCustomerDetailsButton').toggle();
+	$('#deleteCustomer').toggle();
 }
 
 $(document).ready(function(){
 	$('div[id="customerDiv"]').hide();
-	$('div[id="deleteDiv"]').hide();
+	$('div[id="deleteFormDiv"]').hide();
 	$('.topnav a').removeClass('active');
     $('#staffPage').addClass('active');
     //button that show customers and change the other buttons behaviour
-	$('#showCustomersButton').click(showAllCustomers)
+	$('#showClientsButton').click(showAllCustomers)
+	$('#showCustomerDetailsButton').hide();
 	$('#deleteCustomer').hide();
-	$('#deleteCustomer').click(deleteCustomers);
+	$('#updateForm').hide();
+	$('#updateCustomer').hide();
+	
+	$('#deleteCustomer').click(deleteCustomer);
+	$('#showCustomerDetailsButton').click(findById);
+	$('#updateCustomer').click(updateCustomer);
+	$('#customerID').val("");
 	
 });
